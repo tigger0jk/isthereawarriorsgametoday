0d5ba8c1ea834daf4e403e488736e10962bfd8df
Patrick Fay <pfay.us@gmail.com>
2019-08-14 04:37:39 +0000

    Updating site.js to handle NBA data, mostly functional now

--- js/site.js
+++ js/site.js
@@ -1,7 +1,17 @@
-var ballparkName = 'Oracle Park';
+var teamName = 'Warriors';
+// TODO test paths, home away, game today, no game today, etc.
 
 function isDateLaterThan(a, b) {
-  return a > b;
+  if(a.getYear() > b.getYear()) {
+    return true;
+  }
+  if(a.getMonth() > b.getMonth()) {
+    return true;
+  }
+  if(a.getDate() > b.getDate()) {
+    return true;
+  }
+  return false;
 }
 
 /* from https://developer.mozilla.org/en/JavaScript/Reference/Global_Objects/Date */
@@ -10,23 +20,51 @@ function ISODateString(d){
     return d.getUTCFullYear()+'-'+ pad(d.getUTCMonth()+1)+'-'+ pad(d.getUTCDate());
 }
 
+function GetFormattedDate(date) {
+    month = date.getMonth() + 1;
+    dayOfMonth = date.getDate();
+    return month+ "/" + dayOfMonth;
+}
+
+function GetFormattedDateWithWeekday(date) {
+  var weekdays = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"];
+  dayOfWeek = weekdays[date.getDay()];
+  return dayOfWeek + ", " + GetFormattedDate(date);
+}
+
+function GetTimeFromDate(date) {
+    hour = date.getHours() + 1;
+    minute = date.getMinutes();
+    period = "am";
+    if (hour > 12) {
+      hour = hour % 12;
+      period = "pm";
+    }
+    return hour + ":" + minute + period;
+}
+
 $(document).ready(function(){
-    var url = 'data/giants2019schedule.json';
+    var url = 'data/' + teamName + '_schedule.json';
 
     var today = new Date();
+    // TODO remove some of these
+    // var today = new Date(2019, 9, 24, 0, 0, 0, 0); //first game day for testing
+    // var today = new Date(2019, 9, 25, 0, 0, 0, 0); //after first game day for testing
+    // var today = new Date(2019, 9, 27, 8, 0, 0, 0); //second game day for testing
     var nextGame = null;
     var todaysGame = null;
 
+    // TODO clean this up?
     // Format date as MM/DD/YY
-    var curr_date = today.getDate();
-    var curr_month = today.getMonth() + 1;
-    var curr_year = today.getFullYear();
+    // var curr_date = today.getDate();
+    // var curr_month = today.getMonth() + 1;
+    // var curr_year = today.getFullYear();
     // var dateString = curr_month + "/" + curr_date + "/" + curr_year;
 
     // Create datepicker
     // $("#datecheck").html('Checking <input id="datepicker" type="text">');
     // $("#datepicker").datepicker();
-
+// 
     // $(".datepicker").datepicker.("setDate", dateString);
 
     // Check for game today
@@ -34,65 +72,75 @@ $(document).ready(function(){
         var nextGameDate;
 
         $.each(json.games,function(i,game){
-            nextGameDate = new Date(game.date);
+            game.date = new Date(game.d);
+            game.time = GetTimeFromDate(game.date);
+            nextGameDate = game.date;
 
             // Uncomment for debugging
-            //console.log("Today: " + today + " - Looking at game: " + nextGameDate);
+            console.log("Today: " + today + " - Looking at game: " + nextGameDate);
 
           if (!nextGame && isDateLaterThan(nextGameDate, today)){
             nextGame = game;
+            console.log("set first game: ");
+            console.log(nextGame);
             return false; // break the loop
           }
 
             if(today.getYear() == nextGameDate.getYear() && today.getMonth() == nextGameDate.getMonth() && today.getDate() == nextGameDate.getDate()) {
               todaysGame = game;
               nextGame = json.games[i+1];
+              nextGame.date = new Date(nextGame.d);
+              nextGame.time = GetTimeFromDate(nextGame.date);
+              console.log("set a different game: ");
+              console.log(todaysGame);
+              console.log(nextGame);
               return false; // break the loop
             }
         });
 
-        var weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"];
 
         if (todaysGame) {
             $(".fill-in").text("YES");
-            $("#game .summary").text("Warriors play the " + todaysGame.opponent);
-            $("#game .location").text(todaysGame.location);
+            $("#game .location").text(todaysGame.an);
             $("#game .tstart").text(todaysGame.time);
 
-            $("#game abbr").attr('title', ISODateString(nextGameDate));
-            if (todaysGame.location.includes(ballparkName)) {
+            $("#game abbr").attr('title', ISODateString(nextGame.date));
+            if (todaysGame.hn.includes(teamName)) {
+                $("#game .summary").text(teamName + " play the " + todaysGame.hn);
                 $("body").addClass("home");
                 $("#yesno .homeaway").text("At home");
              }
              else {
+                $("#game .summary").text(teamName + " play the " + todaysGame.vn);
                 $("body").addClass("away");
                 $("#yesno .homeaway").text("Away");
                 $("#yesno").css("border-color", "#000");
              }
             $("#game").show();
 
-	    //add small text for next game
-            $("#next .next-location").text(nextGame.location);
+            //add small text for next game
+            $("#next .next-location").text(nextGame.an);
             $("#next .nstart").text(nextGame.time);
-            nextGameDate = new Date(nextGame.date);
-            $("#next .next-day").text(weekday[nextGameDate.getDay()] + ", " + (nextGameDate.getMonth()+1) + "/" + nextGameDate.getDate());
+            $("#next .next-day").text(GetFormattedDateWithWeekday(nextGame.date));
             $("#next").show();
-            
+
         }
         else {
           $(".fill-in").text("NO");
-          $("#game .date").text(nextGame.date);
-          $("#game .summary").text("Warriors will play the " + nextGame.opponent);
-          $("#game .location").text(nextGame.location);
+          // console.log(ISODateString(nextGame.date));
+          // $("#game .date").text(ISODateString(nextGame.date)); // TODO this is unused??
+          if (nextGame.hn.includes(teamName)) {
+            // home game
+            $("#game .summary").text(teamName + " will play the " + nextGame.vn);
+            // $("#nextgame .location").addClass("homegame");
+            // $("body").addClass("homegame-bg");
+          } else {
+            $("#game .summary").text(teamName + " will play the " + nextGame.hn);
+          }
+          $("#game .location").text(nextGame.an);
 
-          // Format next game date as day of the week
-          var nextGameDay = weekday[nextGameDate.getDay()];
-          $("#game .day").text("on " + nextGameDay);
+          $("#game .day").text("on " + GetFormattedDateWithWeekday(nextGame.date));
           $("#game .tstart").text(nextGame.time);
-          // if (nextGame.location == ballparkName) {
-          //  $("#nextgame .location").addClass("homegame");
-          //   $("body").addClass("homegame-bg");
-          // }
           $("#game").show();
         }
     });
